11ed26cb412c0f84a7421780a750fa89a3ff87ec
Apo08K <aroranik452@gmail.com>
2024-10-25 02:28:59 +0530

    logger file updated

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from flask import Flask
+from housing.logger import logging
 
 app = Flask(__name__)
 
 @app.route("/",methods=["GET","POST"]) #get means the parametrs appear in the url and in post it goes to the headers and it doesnt appear in the url. so thats the change. 
 def index():
+    logging.info(" testing logging file")
     return "Hello world"
 
 
---
+++ housing/exception/__init__.py
@@ -0,0 +1,6 @@
+import os
+import sys
+
+class HousingException(Exception): #defining custom exception and Exception is the parent class
+
+    def __init__(self, error_message)
