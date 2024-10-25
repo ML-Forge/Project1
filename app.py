c76180a27c09e26dda3d9a46f5ee814484bb6272
Apo08K <aroranik452@gmail.com>
2024-10-26 01:01:22 +0530

    debugging

--- app.py
+++ app.py
@@ -7,15 +7,15 @@ app = Flask(__name__)
 
 @app.route("/",methods=["GET","POST"]) #get means the parametrs appear in the url and in post it goes to the headers and it doesnt appear in the url. so thats the change. 
 def index():
-    # try:
-    #     raise Exception("We are testing the exception")
+    try:
+        raise Exception("We are testing the exception")
     
-    # except Exception as e:
-    #     # housing = HousingException(e,sys) #created an object of the class
+    except Exception as e:
+         housing = HousingException(e,sys) #created an object of the class
     #     raise HousingException(e,sys) from e
 
-        # logging.info(housing.error_message) #housing is an object and error_message is the attribute of the class HousingException
-        # logging.info(" testing logging file")
+         logging.info(housing.error_message) #housing is an object and error_message is the attribute of the class HousingException
+         logging.info(" testing logging file")
 
 
     return "Hello world"
