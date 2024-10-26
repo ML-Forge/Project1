16dcb8ac066d979d606e975fcbdb6ab075d10b4f
Apo08K <aroranik452@gmail.com>
2024-10-26 22:22:52 +0530

    data ingestion

---
+++ housing/config/__init__.py
@@ -0,0 +1,1 @@
+# input if data ingestion would be taken from s3 bucket or whatver ie. called the data ingestion configuration. 
--- housing/entity/__init__.py
+++ housing/entity/__init__.py
@@ -1,1 +1,4 @@
 # creates the artifact(some file created while running the pipeline) for every component of pipeline.
+# basically all the outputs/artifact generated from every component of the pipeline is stored in this file. 
+
+
