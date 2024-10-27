c8ea81e9177fdc2ae687a85ecc563842455e6914
Apo08K <aroranik452@gmail.com>
2024-10-27 19:53:08 +0530

    pushed the code

--- housing/config/configuration.py
+++ housing/config/configuration.py
@@ -2,6 +2,9 @@
 from housing.entity.config_entity import DataIngestionConfig, DataTransformationConfig,DataValidationConfig,   \
 ModelTrainerConfig,ModelEvaluationConfig,ModelPusherConfig,TrainingPipelineConfig
 
+from housing.util.util import read_yaml_file
+from housing.constant import *
+
 
 
 
@@ -10,8 +13,13 @@ ModelTrainerConfig,ModelEvaluationConfig,ModelPusherConfig,TrainingPipelineConfi
 
 class Configuration:
 
-    def __init__(self) -> None:
-        pass
+    def __init__(self,
+                 config_file_path:str = CONFIG_FILE_PATH,
+                 current_time_stamp:str = CURRENT_TIME_STAMP ) -> None:
+        
+        self.config_info = read_yaml_file(file_path=config_file_path)
+        self.training_pipeline_config = self.get_training_pipeline_config()
+        self.time_stamp = current_time_stamp
 
 
     def get_data_ingestion_config(self) -> DataIngestionConfig: #when this function is called we get this "DataIngestionConfig" entity.
